d667a15968277f6e275ce10ee5acddeb7f710032
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-03-25 14:56:48 +1300

    Complete variable costs (no error handling)

--- C_04_variable_cost.py
+++ C_04_variable_cost.py
@@ -45,14 +45,51 @@ def currency(x):
 # sets up dictionaries and lists
 
 item_list = []
-quantity_list = []
+quant_list = []
 price_list = []
+cost_list = []
 
 variable_dict = {
     "Item": item_list,
-    "Quantity": quantity_list,
+    "Quantity": quant_list,
     "Price": price_list,
+    "Cost": cost_list,
+
 }
+
+while True:
+    # gets the item, quantity, price and cost of each item
+    item = not_blank("Item: ")
+
+    # breaks out of the loop if the user chooses to end early
+    if item.lower() == "xxx":
+        break
+
+    quantity = num_check("Quantity: ", "Please enter an whole number:", int)
+    price = num_check("Price per item: ", "Please enter a number greater than 0:", float)
+    cost = quantity * price
+
+    # appends the inputted values into their lists
+    item_list.append(item)
+    quant_list.append(quantity)
+    price_list.append(price)
+    cost_list.append(cost)
+
+# sets up the table of data
+data_table = pandas.DataFrame(variable_dict)
+data_table = data_table.set_index('Item')
+
+
+# currency formatting (uses currency function)
+add_dollars = ['Price', 'Cost']
+# adds the currency formatting to the strings in the list
+for var_item in add_dollars:
+    data_table[var_item] = data_table[var_item].apply(currency)
+
+print("----------------------")
+
+print(data_table)
+
 
 
 
